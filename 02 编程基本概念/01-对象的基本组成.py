0eeb94393fa5d1bb0001c1bb0b6c515605dd50ed
CJiaの用心 <2224693191@qq.com>
2025-03-17 14:36:39 +0800

    3.17 编程的基本组成-对象的基本组成

---
+++ 02 编程基本概念/01-对象的基本组成.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Date：2025/3/17 14:33:38
+# @Author：CJiaの用心
+# @FileName：01-对象的基本组成.py
+# @Editor：PyCharm2024
+# @Remark：
+a = 3
+print(a)
+print(id(a))
+print(type(a))
+
+b = "你爱我"
+print(b)
+print(id(b))
+print(type(b))
